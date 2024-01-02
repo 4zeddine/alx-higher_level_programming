35d65da277766a9d4e8b10c084cf8cee124d24ad
4zeddine <azeddine.nader@gmail.com>
2024-01-02 12:37:42 -0800

    adding files

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * check_cycle - check for cycle in List
+ * @head: head of linked list
+ *
+ * Return: 1 if cycled, 0 if not
+ */
+
+int check_cycle(listint_t *head)
+{
+	listint_t *n1, *n2;
+
+	if(!head)
+		return (0);
+	n1 = head;
+	n2 = head->next;
+	while (n1 && n2 && n1->next)
+	{
+		if (n2 == n1)
+			return (1);
+		n2 = n2->next;
+		n1 = n1->next->next;
+	}
+	return (0);
+}
