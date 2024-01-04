85a79e24052991c0cf9bb2e59e0b77ef91b02b20
4zeddine <azeddine.nader@gmail.com>
2024-01-04 11:29:24 -0800

    adding files

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,7 +3,7 @@
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     import sys
-    
+
     argv = sys.argv
     if len(argv) - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
