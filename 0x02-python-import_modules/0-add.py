32990b93a607488c09b327ceca5f269026c7f753
4zeddine <azeddine.nader@gmail.com>
2024-01-04 09:44:42 -0800

    adding files

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-import add_0
-a = 1
-b = 2
-print("1 + 2 = {:d}".format(add_0.add(a, b)))
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
