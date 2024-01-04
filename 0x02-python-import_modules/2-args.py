3ac0c5ee89417e0621c49ca605248cc954d96031
4zeddine <azeddine.nader@gmail.com>
2024-01-04 10:41:50 -0800

    adding file

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-import sys
-argv = sys.argv
-i = len(argv)
-j = 1
+if __name__ == "__main__":
+    import sys
 
-if len(argv) == 1:
-    print("0 arguments.")
-elif len(argv) == 2:
-    print("1 argument:")
-    print("1: {}".format(argv[1]))
-else:
-    print("{}: arguments:".format(i - 1))
-    while j < i:
-        print("{}: {}".format(j, argv[j]))
-        j += 1
+    argv = sys.argv
+    i = len(argv)
+    j = 1
+    if len(argv) == 1:
+        print("0 arguments.")
+    elif len(argv) == 2:
+        print("1 argument:")
+        print("1: {}".format(argv[1]))
+    else:
+        print("{}: arguments:".format(i - 1))
+        while j < i:
+            print("{}: {}".format(j, argv[j]))
+            j += 1
