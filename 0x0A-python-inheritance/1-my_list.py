4dee286e39fca31568b1aa38e49fb2c8a2bd13dc
4zeddine <azeddine.nader@gmail.com>
2024-02-05 10:54:08 -0800

    first commit

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defines a subclass of list"""
+
+
+class MyList(list):
+    """subclass of list"""
+    def __init__(self):
+        """initializes the instance"""
+        super().__init__()
+    
+    def print_sorted(self):
+        """prints a sorted list"""
+        print(sorted(self))
