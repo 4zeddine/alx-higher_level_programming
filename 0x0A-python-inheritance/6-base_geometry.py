767f5edeea8211654ef0c11b32bb9dfd457bed77
4zeddine <azeddine.nader@gmail.com>
2024-02-05 11:27:37 -0800

    first commit

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines an empty class BaseGeometry."""
+
+
+class BaseGeometry:
+    """base geometry empty class."""
+
+    def area(self):
+        """raises an exeption."""
+        raise Exception("area() is not implemented")
