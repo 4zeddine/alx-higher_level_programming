890508288998b55c7988c6ee9d58f1726b8c3fc7
4zeddine <azeddine.nader@gmail.com>
2024-02-05 12:50:25 -0800

    first commit

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Defines a class Square subclass of Rectangle class"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """defines a Square"""
+    def __init__(self, size):
+        """initializes a Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(self.__size, self.__size)
+
+    def area(self):
+        """returns an area."""
+        return super().area()
+
+    def __str__(self):
+        """returns a printable string."""
+        return "[Square] {}/{}".format(self.__size, self.__size)
