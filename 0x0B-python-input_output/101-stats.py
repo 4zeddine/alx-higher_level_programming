a3390208a4553a3c60bcbd549e92ef7b30c0f5c5
4zeddine <azeddine.nader@gmail.com>
2024-02-06 15:18:34 -0800

    first commit

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-"""Reads stdin line by line and computes metrics."""
+"""reads stdin line by line and computes metrics."""
 
 
-def print_status(size, status_codes):
-    """prints accumulated metrics.
+def print_stats(size, status_codes):
+    """prints the accumulated metrics.
 
     Args:
-        size (int): read file size.
-        status_codes (dict): count of status codes.
+        size (int): The read file size.
+        status_codes (dict): The count of status codes.
     """
     print("File size: {}".format(size))
     for key in sorted(status_codes):
@@ -18,14 +18,14 @@ if __name__ == "__main__":
     import sys
 
     size = 0
+    count = 0
     status_codes = {}
     valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
-    count = 0
 
     try:
         for line in sys.stdin:
             if count == 10:
-                print_status(size, status_codes)
+                print_stats(size, status_codes)
                 count = 1
             else:
                 count += 1
@@ -46,8 +46,8 @@ if __name__ == "__main__":
             except IndexError:
                 pass
 
-        print_status(size, status_codes)
+        print_stats(size, status_codes)
 
     except KeyboardInterrupt:
-        print_status(size, status_codes)
+        print_stats(size, status_codes)
         raise
