8d34e78df2e2f775c219f7d46438b0f50c0e5699
4zeddine <azeddine.nader@gmail.com>
2024-02-06 11:55:25 -0800

    first commit

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -5,5 +5,5 @@ import json
 
 def load_from_json_file(filename):
     """creates an Object from a “JSON file”"""
-    with open(filename, "w") as f:
+    with open(filename) as f:
         return json.loads(f)
