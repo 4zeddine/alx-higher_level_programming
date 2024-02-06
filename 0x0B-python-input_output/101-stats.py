aba2265a2681c14f9f40161d28097bf0fbffea24
4zeddine <azeddine.nader@gmail.com>
2024-02-06 15:29:26 -0800

    first commit

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,20 +1,16 @@
 #!/usr/bin/python3
-"""reads stdin line by line and computes metrics."""
+"""reads stdin line by line and computes metrics"""
+
+
 import sys
+
 
 if __name__ == "__main__":
     size = [0]
     codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 
-    def print_stats():
-        """prints the accumulated stats."""
-        print("File size: {}".format(size[0]))
-        for key in sorted(codes.keys()):
-            if codes[key]:
-                print("{}: {}".format(key, codes[key]))
-
     def check_match(line):
-        """checks for a regexp match in line."""
+        """Checks for regexp match in line."""
         try:
             line = line[:-1]
             words = line.split(" ")
@@ -22,9 +18,15 @@ if __name__ == "__main__":
             code = int(words[-2])
             if code in codes:
                 codes[code] += 1
-        except:
+        except Exception as e:
             pass
 
+    def print_stats():
+        """Prints accumulated statistics."""
+        print("File size: {}".format(size[0]))
+        for k in sorted(codes.keys()):
+            if codes[k]:
+                print("{}: {}".format(k, codes[k]))
     i = 1
     try:
         for line in sys.stdin:
